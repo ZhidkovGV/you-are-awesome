6f7aca00b296a5e078f82af24d5de1f57512643d
root <zhidkob.gleb@gmail.com>
2018-02-26 00:36:10 +0300

    50% ready

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = () => {return {property: 'value'}};
+const createNotEnumerableProperty = (a) => {
+    Object.defineProperty(Object.prototype, a, {enumerable: false, value: 'value'})
+    return a
+};
+const createProtoMagicObject = () => {
+    return Function
+};
+let counter = 0;
+const incrementor = () => {
+    counter++
+    return incrementor
+};
+incrementor.__proto__.valueOf = function () {
+    return counter;
+}
+coun = 0;
+const asyncIncrementor = () => {
+    return new Promise((resolve) => {
+        setTimeout(()=>{coun ++; resolve(coun)}, 0);
+    })
+};
+let count = 0
+const createIncrementer = () => {
+    return {
+        next : () => {
+            return {value: ++count}
+        },
+        [Symbol.iterator]: function* () {
+            count++
+            yield count;
+        }
+    }
+};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
